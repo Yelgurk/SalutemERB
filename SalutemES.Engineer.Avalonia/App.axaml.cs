1612f48e55c40d5ab0777158ce9ee07668646d47
Maxim <maksim.krugley@bk.ru>
2023-07-07 12:30:11 +0300

    list box items design completed

--- SalutemES.Engineer.Avalonia/App.axaml.cs
+++ SalutemES.Engineer.Avalonia/App.axaml.cs
@@ -1,8 +1,13 @@
 using Avalonia;
+using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Controls.Presenters;
 using Avalonia.Data.Core;
 using Avalonia.Data.Core.Plugins;
+using Avalonia.Input;
 using Avalonia.Markup.Xaml;
+using Avalonia.Media;
+using Avalonia.VisualTree;
 using SalutemES.Engineer.Avalonia.ViewModels;
 using SalutemES.Engineer.Avalonia.Views;
 
@@ -10,6 +15,26 @@ namespace SalutemES.Engineer.Avalonia
 {
     public partial class App : Application
     {
+        private Control? GradientListBoxItemContent = null;
+
+        public void ListBoxItemBorderGradientHandler(object? sender, PointerEventArgs args)
+        {
+            if (sender is not null && (sender as Border)?.GetVisualParent() is Grid listBoxItem)
+            {
+                Point mouse = args.GetPosition(listBoxItem);
+                RadialGradientBrush Accent = (Resources["ListItemBorderRadialGradientAccent"] as RadialGradientBrush)!;
+                RadialGradientBrush Base = (Resources["ListItemBorderRadialGradientBase"] as RadialGradientBrush)!;
+
+                Base.Center = Base.GradientOrigin
+                    = Accent.Center = Accent.GradientOrigin
+                    = new RelativePoint(mouse.X / listBoxItem.Bounds.Width, mouse.Y / listBoxItem.Bounds.Height, RelativeUnit.Relative);
+
+                GradientListBoxItemContent = (sender as Border)!.Child;
+                (sender as Border)!.Child = null;
+                (sender as Border)!.Child = GradientListBoxItemContent;
+            }
+        }
+
         public override void Initialize()
         {
             AvaloniaXamlLoader.Load(this);
--- SalutemES.Engineer.Avalonia/Views/MainWindow.axaml.cs
+++ SalutemES.Engineer.Avalonia/Views/MainWindow.axaml.cs
@@ -11,23 +11,6 @@ namespace SalutemES.Engineer.Avalonia.Views
 {
     public partial class MainWindow : Window
     {
-        private object? GradientListBoxItemContent = null;
-
-        void ListBoxBorderGradientHandler(object? sender, PointerEventArgs args)
-        {
-            if (sender is not null && (sender as Control)?.GetVisualParent() is ContentPresenter listBoxItem)
-            {
-                Point mouse = args.GetPosition(listBoxItem);
-                (Resources["gradRadial"] as RadialGradientBrush)!.Center =
-                (Resources["gradRadial"] as RadialGradientBrush)!.GradientOrigin =
-                new RelativePoint(mouse.X / listBoxItem.Bounds.Width, mouse.Y / listBoxItem.Bounds.Height, RelativeUnit.Relative);
-
-                GradientListBoxItemContent = listBoxItem.Content;
-                listBoxItem.Content = "";
-                listBoxItem.Content = GradientListBoxItemContent;
-            }
-        }
-
         public MainWindow()
         {
             InitializeComponent();
